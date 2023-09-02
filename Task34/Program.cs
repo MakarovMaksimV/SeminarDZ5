e387d4913a197e413ef5d83473260373940f0230
Maksim <mirror0791@mail.ru>
2023-09-02 22:29:33 +0300

    Task34

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+
+Console.Write("Введите кол-во элементов в массиве: ");
+int[] arr = GetRandomArray(Convert.ToInt32(Console.ReadLine()));
+PrintArray(arr);
+Console.WriteLine();
+Console.WriteLine("Количество чётных элементов массива равно: " + FindSumEvenNumbers(arr));
+
+int FindSumEvenNumbers(int[] array)
+{   
+    int count = 0;   
+    for (int i=0; i < array.Length; i++)
+    {
+         if (array[i] % 2 == 0)
+            {
+                count = count + 1;
+            }
+    }
+    return count;        
+}
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(100, 1000);
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int iteam in arr)
+    {
+        Console.Write(iteam + " ");
+    }
+}

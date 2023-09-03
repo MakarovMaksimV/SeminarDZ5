aa17f5ba91093d8b37baa858676ac12955d620ee
Maksim <mirror0791@mail.ru>
2023-09-03 15:53:44 +0300

    Task38 v1.1

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -3,39 +3,68 @@
 [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76 */
 
 Console.Write("Введите кол-во элементов в массиве: ");
-int[] arr = GetRandomArray(Convert.ToInt32(Console.ReadLine()));
+double[] arr = GetRandomArray(Convert.ToInt32(Console.ReadLine()));
 PrintArray(arr);
 Console.WriteLine();
-Console.WriteLine(FindMaxNumber(arr));
+Console.WriteLine("Максимальный элемент массива равен: " + "{0:0.000}", FindMaxNumber(arr));
+Console.WriteLine("Минимальный элемент массива равен: " + "{0:0.000}", FindMinNumber(arr));
+Console.WriteLine("Разница между максимальным и минимальным элементами массива равна: " + "{0:0.000}", (FindMaxNumber(arr) - FindMinNumber(arr)));
 
 
-int FindMaxNumber(int[] array)
+double FindMaxNumber(double[] array)
 {   
-    int maxNumber = array[0];
+    double maxNumber = array[0];
+    
     for (int i=0; i < array.Length; i++)
     {    
         if (array[i] > maxNumber)
             {
-                array[i] = maxNumber;    
+                maxNumber = array[i];     
             }  
     }
     return maxNumber;        
 }
 
-int[] GetRandomArray(int length)
+double FindMinNumber(double[] array)
+{   
+    double minNumber = array[0];
+
+    for (int i=0; i < array.Length; i++)
+    {    
+        if (array[i] < minNumber)
+            {
+                minNumber = array[i];     
+            }  
+    }
+    return minNumber;        
+}
+
+double[] GetRandomArray(int length)
 {
-    int[] result = new int[length];
+    double[] result = new double[length];
+    int maxValue = 166;
+    int minValue = 1;
+
     for (int i = 0; i < length; i++)
     {
-        result[i] = new Random().Next(1, 10);
+        result[i] = new Random().NextDouble() * (maxValue-minValue) + minValue;
     }
     return result;
 }
 
-void PrintArray(int[] arr)
+void PrintArray(double[] arr)
 {
-    foreach (int iteam in arr)
+    foreach (double iteam in arr)
     {
-        Console.Write(iteam + " ");
+        Console.Write("{0:0.000}", iteam);
+        Console.Write(" ");
     }
 }
+
+
+
+
+
+
+
+
